a2d53cc0ecb6488720730046fbd588afb613d29b
Matthieu Melcot <matthieumelcot@hotmail.com>
2020-03-23 23:32:02 +0100

    Fix Continuous Deployment
    Here is the process to create an Heroku app and deploy from the CLI
    $ heroku create hyf-group6-languageplatform   # Create the app
    $ heroku git:remote -a hyf-group6-languageplatform  # Set `heroku` remote
    $ heroku buildpacks:set heroku/nodejs  # Set default language
    $ git push heroku infra/setup-heroku:master     # if 'infra/setup-heroku` is the name of local branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
 
+import React, { Component } from 'react';
+import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>Language Platform</div>
+          <p>
+            {this.state.wow}
+          </p>
+        </header>
+      </div>
+    )
+  }
 
-function App() {
-  return (
-    <div>
-      <h2>Hello World</h2>
-     
-    </div>
-  );
+  state = {
+    wow: ''
+  };
+
+  componentDidMount() {
+    fetch('./api')
+      .then(res => res.json())
+      .then(parsed => {
+        this.setState({ wow: parsed.wow })
+      })
+      .catch(err => console.log(err));
+  }
 }
+
 
 export default App;
