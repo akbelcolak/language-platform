207dc47ed86cf03c002a5c7430c8d31f19e56a95
strik <strike.lier@hotmail.com>
2020-04-06 17:15:59 +0200

    fixed the reloading data form api to user interface then will modify and after that submit back to db

--- client/src/component/admin/addPost.jsx
+++ client/src/component/admin/addPost.jsx
@@ -7,64 +7,73 @@ import { withFormik, Form } from "formik";
 import { FormikTextField, FormikSelectField } from "formik-material-fields";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import * as Yup from "yup";
-const styles = theme => ({
+const styles = (theme) => ({
   container: {
     margin: theme.spacing(3),
-    display:'flex',
-    flexDirection:'row wrap',
-    width:'50%',
-    
+    display: "flex",
+    flexDirection: "row wrap",
+    width: "50%",
   },
   formControl: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
   },
   leftSide: {
     flex: 2,
     height: "100%",
     margin: theme.spacing(1),
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
   },
   rightSide: {
     flex: 1,
     height: "100%",
     margin: theme.spacing(1),
     padding: theme.spacing(3),
-    
-  }
+  },
 });
 class AddPost extends Component {
-    componentDidUpdate(props,state){
-        if(this.props.match.params.view === 'add' && this.props.admin.posts.filter(p=>p.title === this.props.values.title).length > 0){
-            const post = this.props.admin.posts.filter(p=>p.title===this.props.values.title)[0];
-            this.props.history.push('/admin/posts/edit/' + post.dispatch );
-       }
+  componentDidUpdate(props, state) {
+    if (
+      this.props.match.params.view === "add" &&
+      this.props.admin.posts.filter((p) => p.title === this.props.values.title)
+        .length > 0
+    ) {
+      const post = this.props.admin.posts.filter(
+        (p) => p.title === this.props.values.title
+      )[0];
+      this.props.history.push("/admin/posts/edit/" + post.dispatch);
     }
-    componentDidMount(props,state){
-      if(this.props.match.params.view === 'edit' && this.props.match.params.id){
-        this.props.getSinglePost(this.props.match.params.id,this.props.auth.token)
-      }
-
+  }
+  componentDidMount(props, state) {
+    if (this.props.match.params.view === "edit" && this.props.match.params.id) {
+      this.props.getSinglePost(
+        this.props.match.params.id,
+        this.props.auth.token
+      );
     }
+  }
   render() {
     const { classes } = this.props;
     return (
-      <div >
+      <div>
         <Form className={classes.container}>
-          `<Paper className={classes.leftSide}>`
+          `
+          <Paper className={classes.leftSide}>
+            `
             <FormikTextField
               name="title"
               label="Title"
               margin="normal"
-              onChange={e=>this.props.setFieldValue('slug',e.target.value.toLowerCase().replace(/ /g,"_"))}
+              onChange={(e) =>
+                this.props.setFieldValue(
+                  "slug",
+                  e.target.value.toLowerCase().replace(/ /g, "_")
+                )
+              }
               fullWidth
             />
-            <FormikTextField
-              name="slug"
-              label="Slug"
-              margin="normal"
-            />
+            <FormikTextField name="slug" label="Slug" margin="normal" />
             <FormikTextField
               name="content"
               label="Content"
@@ -73,56 +82,79 @@ class AddPost extends Component {
             />
           </Paper>
           <Paper className={classes.rightSide}>
-                <FormikSelectField 
-                name='status'
-                label='Status'
-                margin='normal'
-                options={[
-                  {label:'Published',value:true},
-                  {label:'Unpublished',value:false}
-                ]}
-                fullWidth
-                />
-                <Button variant="contained" color="secondary" onClick={e => {
-                  this.props.handleSubmit()
-                }}>
-                <SaveIcon/> Save</ Button>
+            <FormikSelectField
+              name="status"
+              label="Status"
+              margin="normal"
+              options={[
+                { label: "Published", value: true },
+                { label: "Unpublished", value: false },
+              ]}
+              fullWidth
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={(e) => {
+                this.props.handleSubmit();
+              }}
+            >
+              <SaveIcon /> Save
+            </Button>
           </Paper>
         </Form>
       </div>
     );
   }
 }
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  admin: state.admin
+  admin: state.admin,
 });
-const mapDispatchToProps = dispatch =>({
-    addPost: (post, token)=>{
-        dispatch(AdminActions.addPost(post, token))
-    },
-    getSinglePost: (id, token)=>{
-      dispatch(AdminActions.getSinglePost(id, token))
-  }
+const mapDispatchToProps = (dispatch) => ({
+  addPost: (post, token) => {
+    dispatch(AdminActions.addPost(post, token));
+  },
+  getSinglePost: (id, token) => {
+    dispatch(AdminActions.getSinglePost(id, token));
+  },
 });
-export default withRouter(connect(
-  mapStateToProps,
-   mapDispatchToProps
-   )
-(withFormik({
-  mapPropsToValues: (props) => ({
-    title: props.admin.posts.title ,
-    slug: '',
-    created_at: new Date(),
-    status: false,
-    content:''
-  }),
-  validationSchema: Yup.object().shape({
-    title: Yup.string().required("Title is required"),
-    slug: Yup.string().required("Slug is required"),
-    content: Yup.string().required("Content is required")
-  }),
-  handleSubmit: (values, { setSubmitting, props }) => {
-      props.addPost(values, props.auth.token)
-  }
-})(withStyles(styles)(AddPost))));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(
+    withFormik({
+      mapPropsToValues: (props) => ({
+        title:
+          props.admin.posts.map((i) =>
+            i.id === props.match.params.id ? i.title : null
+          ) || "",
+        slug:
+          props.admin.posts.map((i) =>
+            i.id === props.match.params.id ? i.slug : null
+          ) || "",
+        created_at:
+          props.admin.posts.map((i) =>
+            i.id === props.match.params.id ? i.created_at : null
+          ) || new Date(),
+        status:
+          props.admin.posts.map((i) =>
+            i.id === props.match.params.id ? i.status : null
+          ) || false,
+        content:
+          props.admin.posts.map((i) =>
+            i.id === props.match.params.id ? i.content : null
+          ) || "",
+      }),
+      validationSchema: Yup.object().shape({
+        title: Yup.string().required("Title is required"),
+        slug: Yup.string().required("Slug is required"),
+        content: Yup.string().required("Content is required"),
+      }),
+      handleSubmit: (values, { setSubmitting, props }) => {
+        props.addPost(values, props.auth.token);
+      },
+    })(withStyles(styles)(AddPost))
+  )
+);
