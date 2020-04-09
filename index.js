16146646ebd0f4436ee1490512638f367f5f9ec4
strik <strike.lier@hotmail.com>
2020-04-09 15:30:29 +0200

    fixed json.package syntax

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const api = require('./api/server');
+const api = require('./language-partner/server/server');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/build/index.html');
 });
 app.use('/api', api);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
