7fb4e99096606f6afdb4af214b680235722def4a
strik <strike.lier@hotmail.com>
2020-04-09 16:57:30 +0200

    edited package.json default root

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/build/index.html');
 });
 app.use('/api', api);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
