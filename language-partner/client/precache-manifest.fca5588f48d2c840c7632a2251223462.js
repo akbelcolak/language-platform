5de0647a17a1b86d31b9d46b4d8c093d0913c5b3
strik <strike.lier@hotmail.com>
2020-04-10 14:46:43 +0200

    rebuild react app into server

---
+++ language-partner/client/precache-manifest.fca5588f48d2c840c7632a2251223462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cff54534fe81f327cda3f2d90f71bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb127093c0adacdd59e",
+    "url": "/static/css/2.76af9feb.chunk.css"
+  },
+  {
+    "revision": "8bb127093c0adacdd59e",
+    "url": "/static/js/2.56f44422.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.56f44422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a16e92247325918db1",
+    "url": "/static/js/main.1b903341.chunk.js"
+  },
+  {
+    "revision": "161cca5e93e8a06f76dc",
+    "url": "/static/js/runtime-main.573c3d7a.js"
+  }
+]);
