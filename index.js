89b502beaa7f2701535dd115977993405a56eac3
strik <strike.lier@hotmail.com>
2020-04-12 01:51:30 +0200

    deploy

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // it serves frontend & provides access to our API
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const api = require('./language-partner/server/server');
 const app = express();
@@ -11,10 +13,9 @@ app.use((req, res, next) => {
   console.log('path',req.method + ': ' + req.path);
   next();
 });
-app.use('/', express.static(__dirname + '/client/build'))
-app.get('/', (req, res) => {
-  res.send(__dirname + 'index.html');
-});
+app.use(express.static('/language-partner/client'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/language-partner/client','index.html')));
+
 app.use('/', api);
 const port = process.env.PORT || 61224;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
---
+++ language-partner/client/precache-manifest.fb789c49b4f8f67c551a32d905825ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0734711b595748b047986a08cf9c65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bc0c027bb2bc35b220d",
+    "url": "/static/css/2.27bbd986.chunk.css"
+  },
+  {
+    "revision": "3bc0c027bb2bc35b220d",
+    "url": "/static/js/2.3834193e.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.3834193e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cac7b919d6652158593",
+    "url": "/static/js/main.e743d391.chunk.js"
+  },
+  {
+    "revision": "7921dae39b99aeba8f34",
+    "url": "/static/js/runtime-main.73d2a92c.js"
+  }
+]);
