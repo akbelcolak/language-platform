6af44c4b573cec7a2ac5b12e392407519a7e9e97
strik <strike.lier@hotmail.com>
2020-04-12 21:45:48 +0200

    deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const path = require("path");
 const port = process.env.PORT || 61224;
 const bodyParser = require("body-parser");
-// const api = require("./language-partner/server/server");
+const api = require("./language-partner/server/server");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,18 +16,18 @@ app.use((req, res, next) => {
   next();
 });
 
-
-app.use(favicon(__dirname + '/language-partner/client/favicon.ico'));
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '/language-partner/client')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/language-partner/client', 'index.html'));
 });
-// app.use("/", api);
+
+app.use("/*", api);
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
 
