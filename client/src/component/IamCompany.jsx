a43592836d0b11753315d2130b4147b411e998b7
akbel <colakakbel@gmail.com>
2020-04-17 18:16:52 +0200

    IamCompany page Text

---
+++ client/src/component/IamCompany.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function IamCompany() {
+  return (
+    <div>
+      <div>
+        <h2>SET AN OFFER</h2>
+        <h4>
+          Please set an offer by sending an email to test.cloud123@domain.net
+        </h4>
+        <p>
+          Please write your email in this format:
+          <ul>
+            <li>Subject: Offer Header</li>
+            <li>Company Name</li>
+            <li>Location of the Company</li>
+            <li>Which skills you look for</li>
+            <li>Number of the volunteers you look for</li>
+            <li>The start and end date for the work</li>
+            <li>Any additional note</li>
+          </ul>
+        </p>
+      </div>
+      <div>
+        <h4>By Setting an offer YOU are gaining an opportunity
+          to have a skillfull volunteer for your company! </h4>
+        <h4>User Experiences</h4>
+        <ul>
+          <li>I am a Paula. Thanks to LinguaSphere, I am ready to work in Belgium!
+             Let me tell you how:
+             I had worked for 10 years as Pharmacist in Argantina before I moved to Belgium.
+             My Dutch level is C1 which is pretty enough for any job opportunity in the market yet
+             I didnot have the courege to apply any job application due to lack of mondeling skills.
+             Also I needed to learn the Pharmaceutical Terms in Dutch which is not thought
+             in language courses. With LinguaSphere I have found 2 Pharmacist that matched my profile.
+             I have made my language-intern with one of them and now I am ready for real work!</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- client/src/component/home.jsx
+++ client/src/component/home.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import NavBar from "./navBar";
+//import IamCompany from "./IamCompany";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <h1>Welcome Home</h1>
+        <h1>What is in our home page?</h1>
+        <ul>
+          <li>Login</li>
+          <li> a button directs me SignUp component : I am a language learner and I want to sign up</li>
+          <li> a button directs me IamCompany component: I am a company and I want to set an offer for the language lerners</li>
+          <li>a button directs me * component : to see all companies</li>
+          <li>a button directs me ** component : to see all offers</li>
+        </ul>
+        
       </div>
     );
   }
